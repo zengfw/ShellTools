795266cc4af37ec7f9034a99aa38248a51b00995
Ricky <zfw_email@163.com>
2021-08-24 18:08:06 +0800

    Update personal_utils.sh

--- personal_utils.sh
+++ personal_utils.sh
@@ -1,2 +1,5 @@
-# 文本处理命令
+# 文本处理命令：json格式化文本，过滤出contentType，再进行排序
 cat data.txt | python -m json.tool | grep contentType | sort | uniq
+
+# 搜索目录下某个名称的文件名，并搜索每个文件里头的keyword
+find ./ -name "filename" | xargs grep -rn "keyword" {}
